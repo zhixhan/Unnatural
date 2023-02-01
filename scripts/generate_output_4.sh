26c9b71b1422c039b8487cd85003d90c529070c5
YRdddream <yaruhao@microsoft.com>
2023-02-01 00:09:32 -0800

    output.sh

--- scripts/generate_output_4.sh
+++ scripts/generate_output_4.sh
@@ -1,5 +1,5 @@
 for i in {1..500}
 do
     export AKEY=$(az account get-access-token --resource https://ml.azure.com -o tsv | cut -d $'\t' -f1)
-    python input_generation.py --batch_dir "data/gpt3_generations_4/"
+    python output_generation.py --batch_dir "data/gpt3_generations_4/"
 done
